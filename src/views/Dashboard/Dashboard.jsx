d45035a81d3276ab5d316704d2546b49be767193
Thiemann96 <erictg96@googlemail.com>
2018-11-15 18:55:02 +0100

    port to rechart and 2 axis

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -7,23 +7,17 @@ import { StatsCard } from "components/StatsCard/StatsCard.jsx";
 import 'assets/sass/custom.css'
 import {Checkbox, Radio} from 'react-icheck';
 import {Redirect} from 'react-router-dom'
-var Tooltip = require('chartist-plugin-tooltips')
-var Chartist = require('chartist')
+import {LineChart,Line,CartesianGrid,XAxis,YAxis,Tooltip,Legend,CartesianAxis} from 'recharts'
 
-const options = {
-  axisX: {showGrid:false,
-    showLabel: true,
-  },
-  plugins:[Chartist.plugins.tooltip({
-    class:'custom_tooltip'
-  })]
-}
+
+
+
 var moment = require('moment')
 class Dashboard extends Component {
   constructor(props){
     super()   
     this.state={
-      data:{labels:[],series:[]}, // Series is an Array containing Arrays => data.series[0].push(Array) 
+      data:[], // Array that contains the data that is visualized in the graph 
       selected:"Select Sensor",
       checkboxChecked:false,
       senseBox:props.location.query.content,
@@ -61,15 +55,29 @@ class Dashboard extends Component {
     var filtered = this.state.json.filter((sensor)=>{ // Gives the Measurements which correspond to the checkbox clicked
       return sensor.typ == title;
     })
+    var filtered2 = this.state.json.filter((sensor)=>{
+      return sensor.typ=='Beleuchtungsstärke';
+    })
     var values = []; // Variable needed for storing
-
+    const selected = this.state.selected
     filtered[0].data.map((measurement)=>{ 
       let label = moment(measurement.createdAt).format("DD.MM.YYYY HH:mm")   ;
       let value = parseFloat(measurement.value);
       // Takes the measurements and extracts only the values
-      values.push({meta:label, value:value});
+      values.push({Zeitpunkt:label,Wert:value});
    })
-   values = values.reverse();
+   console.log(filtered2[0].data[2].value)
+   console.log(filtered2[0].data.value)
+
+   var i = 0;
+   var newState = []
+   values.map((tick)=>{
+
+    newState.push({Zeitpunkt:tick.Zeitpunkt,Wert1:tick.Wert,Wert2:parseFloat(filtered2[0].data[i].value)})
+     i +=1;
+    })
+    
+   values = newState.reverse();
    return values
   }
 
@@ -84,6 +92,7 @@ class Dashboard extends Component {
     labels = this.cutArray(20,labels)
     return labels
   }
+
   removeActiveBoxes(){
     const checkedBoxes = document.getElementsByClassName("checked")
     if(checkedBoxes.length>0){
@@ -95,7 +104,14 @@ class Dashboard extends Component {
   componentDidCatch(error, info){
     this.setState({ has_error: true });
   }
-
+  addToGraph(title){
+    var values = this.handleJson(title);
+    values = this.cutArray(20,values);
+    this.setState({
+      data:values,
+      range:"Von " + values[0].Zeitpunkt +" bis " + values[values.length-1].Zeitpunkt
+    })
+  }
 
   // Function that is called whenever a checkbox is clicked 
   handleIsItChecked(e) {
@@ -106,21 +122,14 @@ class Dashboard extends Component {
       this.setState({
         selected:title,
       })
-      // Create Array to push  {labels:[],series:[]}
+      // Create Array to push 
       // For visualization purposes only every 20nd value is displayed
-      var values = this.handleJson(title);
-      values = this.cutArray(20,values);
-      var labels = []   
-      this.setState({
-        data:{labels:labels,series:[values]},
-        range:"Von " + values[0].meta +" bis " + values[values.length-1].meta
-      })
-      console.log(this.state.data);
-
+      
+      this.addToGraph(title);
     }  
     else{
       this.setState({
-        data:{series:[]},selected:"Please Select a sensor"
+        data:{series:[]},selected:"Please Select a sensor",range:"Select a sensor"
       })
     }
     
@@ -158,13 +167,18 @@ class Dashboard extends Component {
                 category={this.state.range}
                 stats="Updated 1 minute ago"
                 content={
-                  <div className="ct-chart" style={{position:'relative'}}>
-                    <ChartistGraph
-                      data={this.state.data}
-                      type="Line"
-                      options={options}
-                      />
-                  </div>
+                    <LineChart width={1500} height={500} data={this.state.data}>
+                      <CartesianGrid stroke="#ccc" />
+                      <YAxis yAxisId={0} />
+                      <YAxis yAxisId={1} orientation="right"/>
+
+                      <XAxis dataKey="Zeitpunkt"/>
+                      <Line yAxisId={0} type="monotone" dataKey="Wert1" stroke="#8884d8"/>
+                      <Line yAxisId={1} type="monotone" dataKey="Wert2" stroke="#4EAF47"/>
+
+                      <Tooltip/>
+                      <Legend/>
+                    </LineChart>
                 }
                 />
             </Col>
