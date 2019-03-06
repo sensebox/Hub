74c31de0253bb3cad4dd0dc81b05308bb8c20914
Thiemann96 <e_thie10@uni-muenster.de>
2019-03-06 18:36:19 +0100

    hopefully this fix does it

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -1,8 +1,5 @@
 import Dashboard from "views/Dashboard/Dashboard";
 import Go from "views/Go/Go"
-import Notifications from "views/Notifications/Notifications";
-import Test from "views/Test/Test"
-import TestOffline from "views/TestOffline/TestOffline"
 import Live from "views/Live/Live"
 const dashboardRoutes = [
   {
@@ -31,20 +28,6 @@ const dashboardRoutes = [
     name: "Dashboard",
     icon: "pe-7s-graph",
     component: Dashboard,
-    shown:false
-  }, 
-  {
-    path: "/test",
-    name: "Test",
-    icon: "pe-7s-graph",
-    component: Test,
-    shown:false
-  }, 
-  {
-    path: "/testoffline",
-    name: "Test Offline",
-    icon: "pe-7s-graph",
-    component: TestOffline,
     shown:false
   }, 
   {
